747830be25edee46ded7305f136c24eb65d66dfb
Michał Żak <michal.zak000@gmail.com>
2024-06-01 23:49:04 +0200

    feat: Add registration functionality to register_view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,10 @@
+import json
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from users.models import CustomUser
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -19,12 +22,36 @@ def login_view(request):
             messages.error(request, 'Invalid email or password.')
     return render(request, 'login.html')
 
-
-
-
+@login_required(login_url='login')
 def logout_view(request):
     logout(request)
     return redirect('login')
 
 def register_view(request):
-    return render(request, "register.html")
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        password = data.get('password')
+        password_repeat = data.get('password_repeat')
+
+        if password != password_repeat:
+            return JsonResponse({'success': False, 'error': 'Passwords do not match'}, status=400)
+
+        if CustomUser.objects.filter(email=email).exists():
+            return JsonResponse({'success': True, 'message': 'User already exists'}, status=200)
+
+        user = CustomUser.objects.create_user(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            is_active=False
+        )
+
+        user.save()
+
+        return JsonResponse({'success': True}, status=201)
+
+    return render(request, 'register.html')
